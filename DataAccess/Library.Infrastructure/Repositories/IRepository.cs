6c2d4b2b026d2392d005951d9e3232777da9a7db
Evgenia <128784367+varinskaevg@users.noreply.github.com>
2025-05-12 14:48:21 +0300

    Lab 3

---
+++ DataAccess/Library.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}

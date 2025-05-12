7e6ad28a238ebb76132f3ad274c1f6271aca50b9
Evgenia <128784367+varinskaevg@users.noreply.github.com>
2025-05-12 15:01:21 +0300

    Lab 3

---
+++ DataAccess/Library.NoSql/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Library.NoSql.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ DataAccess/Library.NoSql/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Library.NoSql.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(string id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(string id);
+    }
+}

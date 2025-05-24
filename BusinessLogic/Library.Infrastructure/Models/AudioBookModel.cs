de397385760e8fe84c0fb65f804060511842e555
Evgenia <128784367+varinskaevg@users.noreply.github.com>
2025-05-24 17:10:54 +0300

    Add files via upload

---
+++ BusinessLogic/Library.Infrastructure/Models/AudioBookModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Infrastructure.Models
+{
+    public class AudioBookModel : BookModel
+    {
+        public string AudioFormat { get; set; }
+        public double DurationInMinutes { get; set; }
+    }
+}
---
+++ BusinessLogic/Library.Infrastructure/Services/CrudServiceAsync.cs
@@ -0,0 +1,59 @@
+﻿using Library.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Infrastructure.Services
+{
+    public class CrudServiceAsync<T> : ICrudServiceAsync<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+
+        public CrudServiceAsync(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> CreateAsync(T element)
+        {
+            await _repository.AddAsync(element);
+            return true;
+        }
+
+        public async Task<T?> ReadAsync(Guid id)
+        {
+            var entity = await _repository.GetAllAsync(); 
+            return entity.FirstOrDefault(e => (e as dynamic).Id == id);
+        }
+
+        public async Task<IEnumerable<T>> ReadAllAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<IEnumerable<T>> ReadAllAsync(int page, int amount)
+        {
+            var all = await _repository.GetAllAsync();
+            return all.Skip((page - 1) * amount).Take(amount);
+        }
+
+        public async Task<bool> UpdateAsync(T element)
+        {
+            await _repository.UpdateAsync(element);
+            return true;
+        }
+
+        public async Task<bool> RemoveAsync(T element)
+        {
+            await _repository.DeleteAsync(element);
+            return true;
+        }
+
+        public Task<bool> SaveAsync()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
